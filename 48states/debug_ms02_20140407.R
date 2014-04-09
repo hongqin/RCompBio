22055976236b4c0fdbc96033ba2b89e746131f59
HongQin <hqinlab@gmail.com>
2014-04-09 11:21:57 -0400

    ms02 update

--- 48states/debug_ms02_20140407.R
+++ 48states/debug_ms02_20140407.R
@@ -8,12 +8,15 @@ net1 = read.csv("48states.csv")
 
 x = ms02_singlerun_v2(net1, indebug=1 )
 y = ms02_singlerun_v2(x, indebug=1 )
-z = ms02_singlerun_v2(y, indebug=1 )
+z = ms02_singlerun(y, indebug=1 )
 
 tx = table(c( x[,1], x[,2]) )
 ty = table(c( y[,1], y[,2]) )
-
 table( tx==ty )
+
+tz = table(c( z[,1], z[,2]) )
+table( tz==ty )
+
 
 
 #double-check
